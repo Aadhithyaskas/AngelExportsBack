009482ff6f6ab046bf0581481c5e0bc250fce691
S Aadhithya <aadhithya503@gmail.com>
2026-07-12 12:43:40 +0530

    Initial commit

--- server.js
+++ server.js
@@ -14,7 +14,11 @@ const app = express();
 
 // Core middleware
 
-app.use(cors());
+app.use(cors({
+  origin: "https://angelexportsback.onrender.com",
+  credentials: true,
+}));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
